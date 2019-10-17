56a354c91a6533b558b30e512eec2844e59225f6
Raighne Weng <raighne.weng@gmail.com>
2019-10-17 23:52:08 +0800

    q2

---
+++ Chapter1/q1-9.scm
@@ -0,0 +1,34 @@
+; F1
+(define (plus a b)
+    (if (= a 0)
+        b
+        (inc (+ (dec a) b) )
+    )
+)
+; (plus 4 5)
+; (inc (plus 3 5))
+; (inc (inc (plus 2 5)))
+; (inc (inc (inc (plus 1 5))))
+; (inc (inc (inc (inc (plus 0 5)))))
+; (inc (inc (inc (inc 5))))
+; (inc (inc (inc 6)))
+; (inc (inc 7))
+; (inc 8)
+; 9
+
+
+; F1
+(define (plus a b)
+    (if (= a 0)
+        b
+        (plus (dec a) (inc b))
+    )
+)
+
+; (plus 4 5)
+; (plus 3 6)
+; (plus 2 7)
+; (plus 1 8)
+; (plus 0 9)
+; 9
+
